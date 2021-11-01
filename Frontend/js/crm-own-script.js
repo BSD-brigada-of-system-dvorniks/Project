a362bb050354ec02afbd15327c305119bb9d4558
Makuta <64552136+Snakemonster@users.noreply.github.com>
2021-11-01 15:03:40 +0200

    added Frontend
    by Makuta

---
+++ Frontend/js/crm-own-script.js
@@ -0,0 +1,43 @@
+function Calendar(id, year, month) {
+    var Dlast = new Date(year, month + 1, 0).getDate(),
+        D = new Date(year, month, Dlast),
+        DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
+        DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+        calendar = '<tr>',
+        month=["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
+    
+    if (DNfirst != 0) {
+      for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
+    }
+    else {
+      for(var  i = 0; i < 6; i++) calendar += '<td>';
+    }
+
+    for(var  i = 1; i <= Dlast; i++) {
+      if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
+        calendar += '<td><a href="#" class="genric-btn primary calendar-table-today">' + i;
+      }
+      else {
+        calendar += '<td><a href="#" class="genric-btn primary calendar-table">' + i;
+      }
+      if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
+        calendar += '<tr>';
+      }
+    }
+    for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+    document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+    document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
+    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
+    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+}
+
+Calendar("calendar", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+
+document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function(){
+  Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) - 1);
+}
+// переключатель плюс месяц
+document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function(){
+  Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) + 1);
+}
