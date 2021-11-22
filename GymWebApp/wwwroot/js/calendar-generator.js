2d14b792a2d4605ee3eee6742ec4dab2e2e63ff8
Makuta <64552136+Snakemonster@users.noreply.github.com>
2021-11-22 16:03:39 +0200

    Added js generator of training list. When click on time modal window show name, date and time when you signed up. Also: -now calendar date are correctly centered in button. -calendars button are no longer <a>. -added choosed day and month in trainig list ,when dowloaded first it show current day and month. -removed some useless css selectors. -updated jquery version.

--- GymWebApp/wwwroot/js/calendar-generator.js
+++ GymWebApp/wwwroot/js/calendar-generator.js
@@ -18,29 +18,33 @@ function Calendar(id, year, month) {
       //   calendar += '<td><a href="#" class="genric-btn calendar-table">' + i;
       // }
       if (i % 3==0 || i%5 ==0){
-        calendar += '<td><a href="#" class="genric-btn calendar-table-training-day" data-toggle="modal" data-target="#modal-training-day">' + i;
+        calendar += '<td><div class="genric-btn calendar-table-training-day" data-toggle="modal" data-target="#modal-training-day"><p>' + i;
       }
       else {
-        calendar += '<td><a href="#" class="genric-btn calendar-table">' + i;
+        calendar += '<td><div class="genric-btn calendar-table" data-month="'+D.getMonth()+'"><p>' + i;
       }
       if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
         calendar += '<tr>';
       }
     }
     for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
-    document.querySelector('#' + id + ' tbody').innerHTML = calendar;
-    document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
-    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
-    document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+
+	$('#'+id+' tbody').html(calendar);
+	$('#'+id+' thead tr:nth-child(1) td:nth-child(2)').html(month[D.getMonth()] + ' ' + D.getFullYear());
+	$('#'+id+' thead tr:nth-child(1) td:nth-child(2)').attr('data-month', D.getMonth());
+	$('#'+id+' thead tr:nth-child(1) td:nth-child(2)').attr('data-year', D.getFullYear());
 }
 
 Calendar("calendar", new Date().getFullYear(), new Date().getMonth());
-// переключатель минус месяц
 
-document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(1)').onclick = function(){
-  Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) - 1);
-}
+// переключатель минус месяц
+$('#calendar thead tr:nth-child(1) td:nth-child(1)').on('click', function(){
+	Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) - 1);
+	// Calendar("calendar", $('#calendar thead tr:nth-child(1) td:nth-child(2)').data('year'), parseFloat($('#calendar thead tr:nth-child(1) td:nth-child(2)').data('month')) - 1);
+});
+
 // переключатель плюс месяц
-document.querySelector('#calendar thead tr:nth-child(1) td:nth-child(3)').onclick = function(){
-  Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) + 1);
-}
+$('#calendar thead tr:nth-child(1) td:nth-child(3)').on('click', function(){
+	Calendar("calendar", document.querySelector('#calendar thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar thead td:nth-child(2)').dataset.month) + 1);
+	// Calendar("calendar", $('#calendar thead tr:nth-child(1) td:nth-child(2)').data('year'), parseFloat($('#calendar thead tr:nth-child(1) td:nth-child(2)').data('month')) + 1);
+});
