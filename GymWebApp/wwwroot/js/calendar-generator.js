87c9c8ae26468030fef957820bfee82d8aa39fc9
Makuta <64552136+Snakemonster@users.noreply.github.com>
2021-11-26 15:45:36 +0200

    Now in trainer list only from today can sign up to training

--- GymWebApp/wwwroot/js/calendar-generator.js
+++ GymWebApp/wwwroot/js/calendar-generator.js
@@ -5,29 +5,32 @@ function Calendar(id, year, month) {
         DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
         calendar = '<tr>',
         month=["January", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
-    
+
     if (DNfirst != 0) {
-      for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
+      for(let i = 1; i < DNfirst; i++) calendar += '<td>';
     }
     else {
-      for(var  i = 0; i < 6; i++) calendar += '<td>';
+      for(let i = 0; i < 6; i++) calendar += '<td>';
     }
 
-    for(var  i = 1; i <= Dlast; i++) {
-      // if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
-      //   calendar += '<td><a href="#" class="genric-btn calendar-table">' + i;
-      // }
-      if (i % 3==0 || i%5 ==0){
+    for(let i = 1; i <= Dlast; i++) {
+
+      if(D.getMonth() == new Date().getMonth() && i < new Date().getDate() || 
+        D.getMonth() < new Date().getMonth() && D.getFullYear() == new Date().getFullYear() ||
+        D.getFullYear() < new Date().getFullYear()) {
+        calendar += '<td><div class="genric-btn disable calendar-disable-day"><p>' + i;
+      }
+      else if (i % 3==0 || i % 5 ==0) {
         calendar += '<td><div class="genric-btn calendar-table-training-day" data-toggle="modal" data-target="#modal-training-day"><p>' + i;
       }
       else {
-        calendar += '<td><div class="genric-btn calendar-table" data-month="'+D.getMonth()+'"><p>' + i;
+        calendar += '<td><div class="genric-btn calendar-table" data-month="'+ D.getMonth() +'"><p>' + i;
       }
       if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
         calendar += '<tr>';
       }
     }
-    for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+    for(let i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
 
 	$('#'+id+' tbody').html(calendar);
 	$('#'+id+' thead tr:nth-child(1) td:nth-child(2)').html(month[D.getMonth()] + ' ' + D.getFullYear());
@@ -36,6 +39,7 @@ function Calendar(id, year, month) {
 }
 
 Calendar("calendar", new Date().getFullYear(), new Date().getMonth());
+console.log(new Date().getDay());
 
 // переключатель минус месяц
 $('#calendar thead tr:nth-child(1) td:nth-child(1)').on('click', function(){
